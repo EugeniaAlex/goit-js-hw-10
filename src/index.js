1751f85e64dbb271a6e87d964c105182eeb9ef4f
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-23 05:27:05 +0200

    task10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import countryCardTpl from './templates/country-card.hbs';
 import countryListTpl from './templates/country-list.hbs';
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+
+// console.log(countryCardTpl);
 
 const DEBOUNCE_DELAY = 300;
 
@@ -9,14 +14,48 @@ const inputSearchEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
-fetch('https://restcountries.com/v3.1/name/peru')
+function onCountryRequest(name) {
+    const FILTER = '?fields=name,capital,population,flags,languages';
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
     .then(response => {
         return response.json();
     })
-    .then(country => {
-        console.log(country);
-        const markup = countryCardTpl(country);
-    })
+    .then(onRenderMarkup)
     .catch(error => {
-        console.log(error);
+        Notify.failure("Oops, there is no country with that name");
     });
+ };
+
+
+inputSearchEl.addEventListener('input', debounce(onCountrySearch, DEBOUNCE_DELAY)); 
+
+function onCountrySearch(event) {
+    event.preventDefault();
+
+    // const inputEv = event.currentTarget;
+    const inputValue = inputSearchEl.value;
+
+    onCountryRequest(inputValue.trim());
+};
+ 
+function onRenderMarkup(country) {
+
+    if (country.length >= 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");    
+    }
+    if (country.length > 2 && country.length < 10) {
+          
+        const markupCountryList = countryListTpl(country);
+
+        countryListEl.innerHTML = markupCountryList;
+        
+
+    }
+     if (country.length === 1) {
+        const markupCountryCard = countryCardTpl(country[0]);
+        countryInfoEl.innerHTML = markupCountryCard;
+        return;
+    }
+    const markup = country.map(el => { return countryListTpl(el);}).join('');
+    countryListEl.innerHTML = markup;
+ };
